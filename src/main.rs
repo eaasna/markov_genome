b1d8c8eb003b485e4852edaa223ea2dae277863a
Evelin <evelin.aasna@fu-berlin.de>
2025-08-05 11:57:08 +0200

    Write output fasta

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use bio::io::fasta::{Reader};
+use bio::io::fasta::{Reader, Writer};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -10,63 +10,85 @@ pub struct Args {
     #[arg(short, long, default_value_t = String::from("seq.fasta"))]
     output: String,
     
-    #[arg(short, long, default_value_t = 100)]
-    seq_len: usize,
+    #[arg(short, long)]
+    lens: Vec<usize>,
+
+    #[arg(long, default_value_t = 3)]
+    order: usize,
+
+    #[arg(short, long, default_value_t = false)]
+    verbose: bool,
 }
 
-fn main() {
-    let args = Args::parse();
-
-    let reader = Reader::from_file(args.input);
-    let records = reader.expect("fasta reader: got an io::Error or could not read_line()").records();
-
-    println!("Input FASTA");
-    let mut rec_in_vec : Vec<Vec<char>> = Vec::new(); 
-    for result in records {
-        let record = result.expect("Error during fasta record parsing");
-        let mut c_vec : Vec<char> = Vec::new();
-        println!("{}", record.id());
-        for i_val in record.seq() {
-            let c_val = match i_val {
+pub fn print_dna4_vec(v : &Vec<Vec<u8>>) {
+    for i in v {
+        for j in i {
+            let c = match j {
                 b'A' | b'a' => 'A',
                 b'C' | b'c' => 'C',
                 b'G' | b'g' => 'G',
                 b'T' | b't' => 'T',
-                _ => panic!("Unknown char\t{:b}", i_val),
+                _ => panic!("Unknown char\t{:b}", j),
             //TODO: read char from int code
             };
-            print!("{}", c_val);
-            c_vec.push(c_val);
+            print!("{}", c);
         }
         println!();
-        rec_in_vec.push(c_vec);
+    }
+}
+fn main() {
+    let mut args = Args::parse();
+
+    if args.lens.len() == 0 {
+        args.lens.push(1000);
+    }
+    let reader = Reader::from_file(args.input);
+    let records = reader.expect("fasta reader: got an io::Error or could not read_line()").records();
+
+    let mut rec_in_vec : Vec<Vec<u8>> = Vec::new(); 
+    for result in records {
+        let record = result.expect("Error during fasta record parsing");
+        rec_in_vec.push(record.seq().to_vec());
+    }
+ 
+    if args.verbose {
+        println!("Input FASTA");
+        print_dna4_vec(&rec_in_vec);
     }
 
-    
     use markov::Chain;
-    let mut chain = Chain::of_order(3);
+    let mut chain = Chain::of_order(args.order);
     // train the chain on some vectors
     for i in 0..rec_in_vec.len() {
         chain.feed(rec_in_vec[i].clone());
     }
 
-    println!("Output FASTA");
     // generate sequences
-    let mut rec_out_vec : Vec<Vec<char>> = Vec::new();
-    
-    let gen = chain.iter_for(args.seq_len);
-    let mut rec_out : Vec<char> = Vec::new();
-    for c in gen {
-        rec_out.push(c[0]);
-        println!("{}", rec_out.len())
-    }
-    rec_out_vec.push(rec_out.clone());
-    
-    for i in 0..rec_out_vec.len() {
-        for j in 0..rec_out_vec[i].len() {
-            print!("{}", rec_out_vec[i][j]);
+    let mut rec_out_vec : Vec<Vec<u8>> = Vec::new();
+
+    for l in args.lens {
+        let mut gen = chain.iter();
+        let mut rec_out : Vec<u8> = Vec::new();
+        for _ in 0..l {
+            let r = gen.next();
+            match r {
+                Some(c) => rec_out.push(c[0]),
+                None    => panic!("Reached iterator end"),
+            }
         }
-        println!();
+        rec_out_vec.push(rec_out.clone());
+    }
+   
+    let mut writer = Writer::to_file(args.output);
+    let mut id = 0;
+    for seq in &rec_out_vec {
+        let _ = writer.as_mut().expect("Error writing record").write(&id.to_string(), None, seq.as_slice());
+        id += 1;
+    }
+    
+    if args.verbose {
+        println!("Output FASTA");
+        print_dna4_vec(&rec_out_vec);
     }
 }
 
